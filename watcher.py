411bf5fea91693c981879e96b16d1e1762e7aa6a
Michael House <michaelhouse@gmx.us>
2017-03-21 16:39:52 -0500

    made watcher threads linear- they will be spawned and updated in series rather than parallel

--- watcher.py
+++ watcher.py
@@ -9,7 +9,7 @@ class Watcher:
 		self.filter = kwargs['filter']
 		self.database = kwargs['database']
 		self.feed = args[0]
-		#self.__get_urls( self.feed )
+		self.etag = ''
 
 	def __get_urls( self, rss ):
 		"""parses urls from feed and updates the database"""
@@ -32,50 +32,28 @@ class Watcher:
 
 		print('found: ' + str(found_cnt) + ' added: ' + str(new_cnt) + ' ignored: ' + str(ignored_cnt))
 
-	def __cycle( self ):
-		"""is executed by the worker thread"""
-		while self.running.is_set():
-			# get a new header
-			header = requests.head(self.feed).headers
+	def update( self ):
+		"""checks to see if the page has changed, possibly updating database"""
+		# get a new header
+		header = requests.head(self.feed).headers
 
-			# check if etag is different and call notify if so
-			if header['etag'] != self.etag:
-				self.etag = header['etag']
-				print('page changed: updating')
-				self.__get_urls( self.feed )
-			else:
-				print('page unchanged')
+		# check if etag is different and call notify if so
+		if header['etag'] != self.etag:
+			print('page changed')
+			self.etag = header['etag']
+			self.__get_urls( self.feed )
+		else:
+			print('page unchanged')
 
-			# don't spam the server
-			time.sleep(10)
 
-	def watch( self, *args, **kwargs ):
-		"""starts a worker watching the target"""
-		# get initial etag value
-		response = requests.head(self.feed)
-		self.etag = response.headers['etag']
-
-		# set 'running' event that will be used to signal exit
-		self.running = threading.Event()
-		self.running.set()
-
-		# create worker and start running
-		self.worker = threading.Thread(target=self.__cycle)
-		self.worker.start()
-
-	def die( self ):
-		"""kills the worker watching the target"""
-		self.running.clear()
-		self.worker.join()
 
 if __name__=='__main__':
 	watcher = Watcher('http://rss.cnn.com/rss/edition.rss', filter=filters.cnn, database='sqlite:///articles.db')
-	watcher.watch()
+	watcher.update()
 
 	try:
 		while True:
-			time.sleep(0.1)
+			time.sleep(10)
+			watcher.update()
 	except KeyboardInterrupt:
-		print('attempting to close thread')
-		watcher.die()
-		print('watcher closed')
+		pass
