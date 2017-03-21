3f54fc90faf71f083f81fb90d3168db756c400c7
Michael House <michaelhouse@gmx.us>
2017-03-21 09:00:45 -0500

    added filters lib and article class

--- article.py
+++ article.py
@@ -1,19 +1,31 @@
 import filters, requests
 
 class Article:
-    def __init__( self, *args, **kwargs ):
-        self.__read( args[0] )
+	def __init__( self, *args, **kwargs ):
+		self.filter = kwargs['filter']
+		self.__read( args[0] )
 
-    def __read( self, url ):
-        response = requests.get( url )
+	def __read( self, url ):
+		response = requests.get( url )
 
-        self.url = url
-        self.raw_content = response.text
-        self.content = self.__parse( response.text )
+		self.url = url
+		self.raw_content = response.text
+		article = self.__parse( response.text )
 
-    def __parse( self, content ):
-        return filters.cnn( content )
+		self.title = article['title']
+		self.author = article['author']
+		self.category = article['category']
+		self.content = article['content']
+
+	def __parse( self, content ):
+		if self.filter:
+			return self.filter( content )
+
 
 
 if __name__=='__main__':
-    article = Article( 'http://www.cnn.com/2017/03/20/politics/james-comey-hearing-white-house-cloud/index.html' )
+	article = Article( 'http://www.cnn.com/2017/03/20/politics/james-comey-hearing-white-house-cloud/index.html', filter=filters.cnn )
+	print(article.title)
+	print(article.author)
+	print(article.category)
+	print(article.content)
--- filters.py
+++ filters.py
@@ -1,24 +1,26 @@
 import re
+from unidecode import unidecode
 from bs4 import BeautifulSoup
 
 def cnn( content ):
-    soup = BeautifulSoup(content, 'html.parser')
+	soup = BeautifulSoup(content, 'html.parser')
 
-    # find the content
-    matches, result = soup.find_all(['div','p'], class_="zn-body__paragraph"), []
-    for match in matches:
-        result.append( ''.join(match.strings) )
+	# find the content
+	matches, result = soup.find_all(['div','p'], class_="zn-body__paragraph"), ''
+	for match in matches:
+		inner_text = match.find(text=True, recursive=False)
+		result += unidecode( inner_text ).strip() + '\n' if inner_text else ''
 
-    # find the author
-    author_meta = soup.find('meta', {'itemprop':'author'})
-    author = author_meta['content'] if author_meta else 'None'
+	# find the author
+	author_meta = soup.find('meta', {'itemprop':'author'})
+	author = author_meta['content'].split(',')[0] if author_meta else 'None'
 
-    # find the title
-    title_meta = soup.find('meta', {'itemprop':'headline'})
-    title = title_meta['content'] if title_meta else 'None'
+	# find the title
+	title_meta = soup.find('meta', {'itemprop':'headline'})
+	title = title_meta['content'] if title_meta else 'None'
 
-    # find the section
-    section_meta = soup.find('meta', {'itemprop':'articleSection'})
-    section = section_meta['content'] if section_meta else 'None'
+	# find the section
+	section_meta = soup.find('meta', {'itemprop':'articleSection'})
+	section = section_meta['content'] if section_meta else 'None'
 
-    return ( section, title, author, result )
+	return {'category':section,'title':title,'author':author,'content':result}
