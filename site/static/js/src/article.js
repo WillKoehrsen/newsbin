2e68359a8f208eefb114b190e955b7baee8400d5
Michael House <michaelhouse@gmx.us>
2017-06-01 06:30:59 -0500

    pre-deploy automated commit

---
+++ site/static/js/src/article.js
@@ -0,0 +1,76 @@
+// -----------------------------------------------------------------------------
+// Localize the datetime on under the title
+var block = document.getElementById('js-localize-date');
+if(block!=null){
+	var date = new Date( block.getAttribute('iso') );
+	block.innerHTML = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+}
+
+// -----------------------------------------------------------------------------
+// customize document prototype
+HTMLDocument.prototype.createElementWithAttr = function( tag, attrs ){
+	var elem = this.createElement(tag);
+	for(attr in attrs){ elem.setAttribute(attr,attrs[attr]); }
+	return elem;
+}
+
+// -----------------------------------------------------------------------------
+// Annotation:
+//		a modal component that displays when an x-annotation is clicked
+var modal = (function( target ){
+	var layout = {
+		modal:document.getElementById( target ),
+		contents:document.createElementWithAttr('div',{class:'modal-container'}),
+		part:{
+			close:document.createElementWithAttr('div',{class:'modal-close',onClick:'modal.close()'}),
+			title:document.createElementWithAttr('div',{class:'modal-title'}),
+			image:document.createElementWithAttr('img',{class:'modal-image'}),
+			content:document.createElementWithAttr('div',{class:'modal-content'}),
+			link:document.createElementWithAttr('a',{class:'modal-link',target:'_blank'}),
+		},
+	}
+	// add content to the modal window
+	layout.modal.appendChild(layout.contents);
+	for(item in layout.part){ layout.contents.appendChild(layout.part[item]); }
+
+	layout.part.link.innerHTML = "wikipedia";
+	layout.part.close.innerHTML = "Close";
+
+	var operations = {
+		display:function( name ){
+			var handle = new XMLHttpRequest();
+			var url = '/annotations?name=' + name;
+			handle.onload = function(){
+				if(this.status==200){
+					var response = JSON.parse(this.responseText);
+					layout.part.title.innerHTML = response.name;
+					layout.part.image.setAttribute('src',response.image);
+					layout.part.content.innerHTML = response.summary;
+					layout.part.link.setAttribute('href','https://en.wikipedia.org/wiki/' + response.name);
+					layout.modal.style.display = "flex";
+				}
+			}
+			handle.open("GET", url, true);
+			handle.send();
+		},
+
+		close:function(){
+			layout.part.title.innerHTML = "";
+			layout.part.image.setAttribute('src',"#");
+			layout.part.content.innerHTML = "";
+			layout.part.link.setAttribute('href','#');
+			layout.modal.style.display = "none";
+		},
+	}
+
+	return operations;
+})( 'js-annotation-modal' );
+
+var annotations = document.getElementsByClassName('annotation');
+if(annotations){
+	for( var i = 0; i < annotations.length; i++ ){
+		annotations[i].addEventListener("click", function(_event){
+			modal.display(this.getAttribute('name'));
+		});
+	}
+}
---
+++ site/static/js/src/index.js
@@ -0,0 +1,141 @@
+// -----------------------------------------------------------------------------
+// implement forEach if we're in IE -.-
+if (!NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function(fn, scope) {
+        for(var i = 0, len = this.length; i < len; ++i) {
+            fn.call(scope, this[i], i, this);
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+// Refill Form
+//      On reloads and navigation without submission, we want to
+//      set the form values to the sessionStorage saved values
+//      so that they don't lose their options.
+window.addEventListener('load',function(){
+    var elements = document.forms[0].elements;
+    for( var i = 0; i < elements.length; i++ ){
+        var obj = elements[i];
+        if(obj.type=='checkbox'){
+            if(obj.name in sessionStorage){
+                obj.checked = (sessionStorage[obj.name]!="false");
+            }
+        }
+        else if(['text','number','select-one'].indexOf(obj.type) >= 0){
+            if(obj.name in sessionStorage){
+                if(obj.type=="number"){
+                    obj.value = parseInt(sessionStorage[obj.name]);
+                } else {
+                    obj.value = sessionStorage[obj.name];
+                }
+            }
+        }
+    }    
+})
+
+// -----------------------------------------------------------------------------
+// Localize the datetime on title-cards
+var dates = document.getElementsByClassName('js-localize-date');
+for( var i = 0; i < dates.length; i++ ){
+    var block = dates[i]
+    var date = new Date( block.getAttribute('iso') );
+    block.innerHTML = date.toLocaleDateString() + " " + date.toLocaleTimeString();
+}
+
+// -----------------------------------------------------------------------------
+//	Layout:
+// 		rather than search for elements in each function/handler
+// 		this layout object gets the searching out of the way up
+// 		front.
+layout = {
+	all:document.getElementById('all-check'),
+	plain:document.getElementById('plain-check'),
+	regex:document.getElementById('regex-check'),
+	form:document.getElementById('js-sidebar-form'),
+	search:document.getElementById('js-search-input'),
+	number:document.getElementById('js-number-input'),
+	category:document.getElementById('js-category-select'),
+	sources:document.querySelectorAll('.sources input:not(#all-check)'),
+}
+
+// -----------------------------------------------------------------------------
+// Checkboxes:
+//		if javascript is enabled, we grab all the checkboxes in the page,
+//		hide them and apply a class that displays an alternate 'checkbox'
+var inputs = document.querySelectorAll('input[type=checkbox]');
+for( var i = 0; i < inputs.length; i++ ){
+	inputs[i].style.display = "none";
+	inputs[i].classList.add('checkable');
+}
+
+// -----------------------------------------------------------------------------
+// Sources:
+// 		on changing source options, unselect/select 'all' appropriately
+for( var i = 0; i < layout.sources.length; i++ ){
+	layout.sources[i].addEventListener('change',function(_event){
+        sessionStorage[this.name] = this.checked;
+		if(this.checked){
+			layout.all.checked = true;
+			layout.sources.forEach(function(source){
+				if(!source.checked){
+					layout.all.checked = false;
+				}
+			});
+		} else {
+			layout.all.checked = false;
+		}
+        sessionStorage[layout.all.name] = layout.all.checked;
+	});
+}
+
+// -----------------------------------------------------------------------------
+// Save Changes (search,number,category)
+//      sessionStorage values are saved for checkboxes in
+//      the other handlers, but we didn't have handlers
+//      for these inputs, so these were added.
+layout.search.addEventListener('change',function(_event){
+    sessionStorage[this.name] = this.value;
+});
+
+layout.number.addEventListener('change',function(_event){
+    sessionStorage[this.name] = this.value;
+});
+
+layout.category.addEventListener('change',function(_event){
+    sessionStorage[this.name] = this.value;
+});
+
+// -----------------------------------------------------------------------------
+// Regex/Plain:
+// 		on 'regex' and 'plain', alternate checks (only one gets submitted)
+layout.plain.addEventListener('change',function(_event){
+	layout.regex.checked = !this.checked;
+    sessionStorage[this.name] = this.checked;
+    sessionStorage[layout.regex.name] = layout.regex.checked;
+});
+
+layout.regex.addEventListener('change',function(_event){
+	layout.plain.checked = !this.checked;
+    sessionStorage[this.name] = this.checked;
+    sessionStorage[layout.plain.name] = layout.plain.checked;
+});
+
+// -----------------------------------------------------------------------------
+// All:
+// 		on 'all' change, select/unselect all sources
+layout.all.addEventListener('change',function(_event){
+	var caller = this;
+	layout.sources.forEach(function(source){
+		source.checked = caller.checked;
+        sessionStorage[source.name] = source.checked;
+	});
+    sessionStorage[this.name] = this.checked;
+});
+
+// -----------------------------------------------------------------------------
+// Form:
+// 		on form submit, preprocess data
+layout.form.addEventListener('submit',function(_event){
+	layout.plain = !layout.regex;												// sanity check
+});
