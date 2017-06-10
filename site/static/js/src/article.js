68346527711b12b810ee3d5bdb48ce828c7e35b0
Michael House <michaelhouse@gmx.us>
2017-06-10 06:58:40 -0500

    still working with vuejs

--- site/static/js/src/article.js
+++ site/static/js/src/article.js
@@ -7,10 +7,8 @@ if(block!=null){
 }
 
 // -----------------------------------------------------------------------------
-// Annotation:
-//		a modal component that displays when an x-annotation is clicked
-//
-var data_set = {
+// VueJS
+var annotation_defaults = {
 	name:'',
 	image:'',
 	table:{},
@@ -19,12 +17,9 @@ var data_set = {
 	politifact_link:'',
 };
 
-var annotation = new Vue({
-
+var modal = new Vue({
 	el: '#modal-annotation',
-
-	data: data_set,
-
+	data: annotation_defaults,
 	methods: {
 		close: function( event ){
 			this.$el.style.display = "none";
@@ -33,60 +28,28 @@ var annotation = new Vue({
 			this.$el.style.display = "flex";
 		},
 		clear: function( event ){
-			Object.assign( this.$data, data_set);
+			Object.assign( this.$data, annotation_defaults);
 		},
 	}
 });
 
-var annotations = document.getElementsByClassName('annotation');
-if(annotations){
-	for( var i = 0; i < annotations.length; i++ ){
-		annotations[i].addEventListener("click", function(_event){
-			var name = this.getAttribute('name');
-			var handle = new XMLHttpRequest();
-			var url = '/annotations?name=' + name;
+var add = new Vue({
+	el: '#js-add-selection',
+	data: { value:''}
+});
 
-			handle.onload = function(){
-				if(this.status==200){
-					var response = JSON.parse(this.responseText);
-					annotation.clear();
-
-					annotation.$data.name = response.name;
-					annotation.$data.image = response.image;
-					annotation.$data.content = response.summary.split('\n\n');
-					if(response.truth_score){
-						annotation.$data.table['truth_score'] = {
-								key:"Truth Score",
-								value:response.truth_score+"%",
-								tooltip:'calculated from the last five statements fact-checked by politifact.com'
-							};
-					}
-					if(response.slug){
-						annotation.$data.politifact_link = "http://www.politifact.com/personalities/" + response.slug;
-					}
-					annotation.$data.wikipedia_link = "https://en.wikipedia.org/wiki/" + response.name;
-
-					annotation.open();
-				}
-			}
-
-			handle.open("GET", url, true);
-			handle.send();
-		});
+var annotation {
+	template: '<x-annotation>{{ name }}</x-annotation>',
+	data: function () {
+		return {};
 	}
 }
 
-var add = new Vue({
-	el: '#js-add-selection',
-	data: {
-		value:'',
-	},
-	methods: {}
-});
-
 var content = new Vue({
 	el: '#js-capture-selection',
-	data: {},
+	components: {
+		'annotation':annotation,
+	},
 	methods: {
 		capture: function( event ){
 			var selection = window.getSelection();
@@ -99,6 +62,46 @@ var content = new Vue({
 		},
 	}
 });
+
+/* -------------------------------------------------------------------------- */
+/* Plain JS */
+var annotations = document.getElementsByClassName('annotation');
+if(annotations){
+	for( var i = 0; i < annotations.length; i++ ){
+		annotations[i].addEventListener("click", function(_event){
+			var name = this.getAttribute('name');
+			var handle = new XMLHttpRequest();
+			var url = '/annotations?name=' + name;
+
+			handle.onload = function(){
+				if(this.status==200){
+					var response = JSON.parse(this.responseText);
+					modal.clear();
+
+					modal.$data.name = response.name;
+					modal.$data.image = response.image;
+					modal.$data.content = response.summary.split('\n\n');
+					if(response.truth_score){
+						modal.$data.table['truth_score'] = {
+								key:"Truth Score",
+								value:response.truth_score+"%",
+								tooltip:'calculated from the last five statements fact-checked by politifact.com'
+							};
+					}
+					if(response.slug){
+						modal.$data.politifact_link = "http://www.politifact.com/personalities/" + response.slug;
+					}
+					modal.$data.wikipedia_link = "https://en.wikipedia.org/wiki/" + response.name;
+
+					modal.open();
+				}
+			}
+
+			handle.open("GET", url, true);
+			handle.send();
+		});
+	}
+}
 
 menu_tab = document.getElementById('js-tab-menu');
 article_tab = document.getElementById('js-tab-article');
