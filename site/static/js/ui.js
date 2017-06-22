303f9f40d688da2ac630f4cc85a0f73e0a8d8218
Michael House <michaelhouse@gmx.us>
2017-06-21 21:42:33 -0500

    pre-deploy automated commit

--- site/static/js/ui.js
+++ site/static/js/ui.js
@@ -24,24 +24,27 @@ var tooltips = (function( targets ){
                 var popup = document.createElement('div');
                 tooltip = popup;
                 popup.classList.add("tooltip");
-                popup.innerHTML = _event.target.getAttribute('tooltip');
-                document.body.appendChild(popup);
+                var msg = _event.target.getAttribute('tooltip');
+                if(msg.trim()){
+                    popup.innerHTML = msg;
+                    document.body.appendChild(popup);
 
-                // move to where the tooltip should display
-                var reference = _event.target.getBoundingClientRect();
-                var right_ref = parseInt(reference.right);
-                var left_ref = parseInt(reference.left);
+                    // move to where the tooltip should display
+                    var reference = _event.target.getBoundingClientRect();
+                    var right_ref = parseInt(reference.right);
+                    var left_ref = parseInt(reference.left);
 
-                if(left_ref < window.innerWidth/2){
-                    popup.style.left = right_ref+window.pageXOffset+offset+'px'
-                    popup.classList.add('left');
+                    if(left_ref < window.innerWidth/2){
+                        popup.style.left = right_ref+window.pageXOffset+offset+'px'
+                        popup.classList.add('left');
+                    }
+                    else {
+                        popup.style.left = left_ref-parseInt(popup.offsetWidth)+window.pageXOffset-offset+'px'
+                        popup.classList.add('right');
+                    }
+
+                    popup.style.top = parseInt(reference.top) + window.pageYOffset + 'px';
                 }
-                else {
-                    popup.style.left = left_ref-parseInt(popup.offsetWidth)+window.pageXOffset-offset+'px'
-                    popup.classList.add('right');
-                }
-
-                popup.style.top = parseInt(reference.top) + window.pageYOffset + 'px';
             });
 
             element.addEventListener('mouseout',function( _event ){
