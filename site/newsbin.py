cbb3026d10601971911737b749c10a635cf18630
Michael House <michaelhouse@gmx.us>
2017-07-09 07:06:19 -0500

    pre-deploy automated commit

--- site/newsbin.py
+++ site/newsbin.py
@@ -112,7 +112,10 @@ def article( pk ):
 					blacklist = article.blacklist.replace(';',', ')
 				else:
 					blacklist = ''
-				summary = regex.sub('<.*?>','',article.content[:160])
+				try:
+					summary = regex.sub('<.*?>','',article.content[:160].replace('</cite>','</cite> '))
+				except:
+					summary = article.content[:160]
 				return render_template('article.html', article=article, blacklist=blacklist, date=datetime.datetime.now(), summary=summary)
 			except Exception as e:
 				log.exception(e)
